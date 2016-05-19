1f9ec8424024f50eaa7ce71fa0b82804cb499321
liwei <24909187@qq.com>
2016-05-19 09:51:40 +0800

    cmdtimer

---
+++ CmdTimer/CmdTimer/Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace CmdTimer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.TrayNotifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 25);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(241, 21);
+            this.textBox1.TabIndex = 0;
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(259, 23);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowse.TabIndex = 1;
+            this.btnBrowse.Text = "browse";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(61, 93);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(257, 93);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "close";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(13, 53);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(62, 21);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.Text = "15";
+            // 
+            // TrayNotifyIcon
+            // 
+            this.TrayNotifyIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("TrayNotifyIcon.Icon")));
+            this.TrayNotifyIcon.Text = "notifyIcon1";
+            this.TrayNotifyIcon.Visible = true;
+            this.TrayNotifyIcon.MouseClick += new System.Windows.Forms.MouseEventHandler(this.TrayNotifyIcon_MouseClick);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(142, 93);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "stop";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(59, 123);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 12);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "已执行次数";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(81, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(17, 12);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "秒";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(344, 144);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Deactivate += new System.EventHandler(this.Form1_Deactivate);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.NotifyIcon TrayNotifyIcon;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ CmdTimer/CmdTimer/Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
+
+namespace CmdTimer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+
+            openFileDialog1.Filter = "bat文件|*.bat";
+            if (DialogResult.OK == openFileDialog1.ShowDialog())
+            {
+                textBox1.Text = openFileDialog1.FileName;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            RunCmd(textBox1.Text.Trim(), false, true);
+            
+            timer1.Interval = Convert.ToInt32(textBox2.Text.Trim()) * 1000;
+            timer1.Start();
+            HideMainForm();
+        }
+
+        int times = 0;
+        protected void RunCmd(String cmd, Boolean showWindow, Boolean waitForExit)
+        {
+            var p = new Process();
+            var si = new ProcessStartInfo();
+            var path = Environment.SystemDirectory;
+            path = Path.Combine(path, @"cmd.exe");
+            si.FileName = path;
+            if (!cmd.StartsWith(@"/")) cmd = @"/c " + cmd;
+            si.Arguments = cmd;
+            si.UseShellExecute = false;
+            si.CreateNoWindow = !showWindow;
+            si.RedirectStandardOutput = true;
+            si.RedirectStandardError = true;
+            p.StartInfo = si;
+
+            p.Start();
+            if (waitForExit)
+            {
+                p.WaitForExit();
+
+                var str = p.StandardOutput.ReadToEnd();
+                if (!String.IsNullOrEmpty(str))
+                {
+                    Console.WriteLine(str.Trim(new Char[] { '\r', '\n', '\t' }).Trim());
+                }
+                str = p.StandardError.ReadToEnd();
+                if (!String.IsNullOrEmpty(str))
+                {
+                    Console.WriteLine(str.Trim(new Char[] { '\r', '\n', '\t' }).Trim());
+                }
+            }
+            times++;
+            label1.Text = "已执行次数:" + times;
+        }
+
+        private void ShowMainForm()
+        {
+            this.Visible = true;                            // 窗体可见
+            this.WindowState = FormWindowState.Normal;      // 窗体状态为正常
+            TrayNotifyIcon.Visible = true;                  // 托盘图标可见
+            this.ShowInTaskbar = true;                      // 在任务栏显示窗体
+        }
+
+        // 隐藏窗体和任务栏图标
+        private void HideMainForm()
+        {
+            this.WindowState = FormWindowState.Minimized;   // 最小化时隐藏窗体
+            this.Visible = false;                           // 窗体不可见
+            this.TrayNotifyIcon.Visible = true;             // 图标在任务栏区域可见
+            this.ShowInTaskbar = false;                     // 不在在任务栏显示窗体
+        }
+
+        private void TrayNotifyIcon_MouseClick(object sender, MouseEventArgs e)
+        {
+            // 判断是否单击鼠标左键
+            if (e.Button == MouseButtons.Left)
+            {
+                // 切换显示状态
+                if (this.WindowState == FormWindowState.Minimized)
+                {
+                    ShowMainForm();
+                }
+                else
+                {
+                    HideMainForm();
+                }
+            }
+
+        }
+
+
+        private void Form1_Deactivate(object sender, EventArgs e)
+        {
+            // 判断是否为最小化状态
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                HideMainForm();
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            RunCmd(textBox1.Text.Trim(), false, true);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+        }
+
+
+
+    }
+}
