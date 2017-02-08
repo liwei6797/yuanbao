89919029a0daf9cef899b53d0e69b6a31df6f2f4
liwei <liwei6797@gmail.com>
2017-02-08 23:10:50 +0800

    openvpn客户端服务

---
+++ OvpnClientService/OVPNClientService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OvpnService
+{
+    public partial class OpenVPNClient : ServiceBase
+    {
+        Process proc;
+        public OpenVPNClient()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            proc = Process.Start("openvpn-gui.exe", "--connect client.ovpn");
+        }
+
+        protected override void OnStop()
+        {
+            //if (proc != null)
+            //{
+            //    proc.Kill();
+            //}
+        }
+    }
+}
