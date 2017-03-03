bf742c5cb6aa8a61c55b695cfd5cbe440870f277
liwei <liwei6797@gmail.com>
2017-03-03 17:50:42 +0800

    openvpn服务 增加参数配置

--- OvpnClientService/OVPNClientService.cs
+++ OvpnClientService/OVPNClientService.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Configuration;
 using System.Data;
 using System.Diagnostics;
 using System.Linq;
@@ -20,7 +21,8 @@ namespace OvpnService
 
         protected override void OnStart(string[] args)
         {
-            proc = Process.Start("openvpn-gui.exe", "--connect client.ovpn");
+            string connectParam = GetAppConfig("--connect");
+            proc = Process.Start("openvpn-gui.exe", "--connect " + connectParam);
         }
 
         protected override void OnStop()
@@ -29,6 +31,51 @@ namespace OvpnService
             //{
             //    proc.Kill();
             //}
+        }
+
+        ///<summary> 
+        ///返回*.exe.config文件中appSettings配置节的value项  
+        ///</summary> 
+        ///<param name="strKey"></param> 
+        ///<returns></returns> 
+        public static string GetAppConfig(string strKey)
+        {
+            string file = System.Windows.Forms.Application.ExecutablePath;
+            Configuration config = ConfigurationManager.OpenExeConfiguration(file);
+            foreach (string key in config.AppSettings.Settings.AllKeys)
+            {
+                if (key == strKey)
+                {
+                    return config.AppSettings.Settings[strKey].Value.ToString();
+                }
+            }
+            return null;
+        }
+
+        ///<summary>  
+        ///在*.exe.config文件中appSettings配置节增加一对键值对  
+        ///</summary>  
+        ///<param name="newKey"></param>  
+        ///<param name="newValue"></param>  
+        public static void UpdateAppConfig(string newKey, string newValue)
+        {
+            string file = System.Windows.Forms.Application.ExecutablePath;
+            Configuration config = ConfigurationManager.OpenExeConfiguration(file);
+            bool exist = false;
+            foreach (string key in config.AppSettings.Settings.AllKeys)
+            {
+                if (key == newKey)
+                {
+                    exist = true;
+                }
+            }
+            if (exist)
+            {
+                config.AppSettings.Settings.Remove(newKey);
+            }
+            config.AppSettings.Settings.Add(newKey, newValue);
+            config.Save(ConfigurationSaveMode.Modified);
+            ConfigurationManager.RefreshSection("appSettings");
         }
     }
 }
---
+++ OvpnClientService/readme.txt
@@ -0,0 +1,4 @@
+﻿需要openvpn路径在环境变量path中
+安装/卸载命令：
+installutil OvpnClientService.exe
+installutil /u OvpnClientService.exe
